a5f85251860aa7b26d72c0a88db7d80a8969182f
hamidreza <takloohamidreza@gmail.com>
2022-10-04 14:36:40 +0330

    Create 2 function for toggle and remove active

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import "./sass/index.sass";
+import { focusedSelect, unFocusedSelect } from "./selectInput";
 
 document.addEventListener("click", (e) => {
   if (!e.target.matches(".select")) {
+    unFocusedSelect();
   } else {
+    focusedSelect();
   }
 });
--- src/selectInput.js
+++ src/selectInput.js
@@ -1,3 +1,13 @@
 const select = document.querySelector(".select");
 const optionBox = document.querySelector(".options");
 const options = [...document.querySelectorAll(".options .item")];
+
+const unFocusedSelect = () => {
+  optionBox.classList.remove("active");
+};
+
+const focusedSelect = () => {
+  optionBox.classList.toggle("active");
+};
+
+export { focusedSelect, unFocusedSelect };
