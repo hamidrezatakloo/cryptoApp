db5e310a578df6243e0176f5f224af872f925d00
hamidreza <takloohamidreza@gmail.com>
2022-10-04 20:32:15 +0330

    Add check mark to select-input
    check mark Toggle  by respect of options

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ import {
 
 startListenOptions();
 
+const checkBox = document.querySelector(".checkbox");
+
 document.addEventListener("click", (e) => {
+  if (e.target.matches(".options>.item")) checkBox.classList.add("checked");
+
   if (!e.target.matches(".select")) {
     unFocusedSelect();
   } else {
+    checkBox.classList.remove("checked");
     focusedSelect();
   }
 });
