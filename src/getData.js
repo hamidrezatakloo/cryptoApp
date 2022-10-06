fce70897fd0796e4fe4225238a5b4c39b30b45bb
hamidreza <takloohamidreza@gmail.com>
2022-10-06 13:28:51 +0330

    Feat: fetch yesterday data for all coins

--- src/getData.js
+++ src/getData.js
@@ -5,3 +5,17 @@ const getYesterday = () => {
   yesterday = yesterday.toJSON().slice(0, 10);
   return yesterday;
 };
+
+const groupedDaily = async () => {
+  const response = await fetch(
+    `https://api.polygon.io/v2/aggs/grouped/locale/global/market/crypto/${getYesterday()}?adjusted=true&apiKey=PeWHlEcjh_T7PvDn5briSyXfXNyjqe28`
+  );
+
+  if (response.status >= 200 && response.status <= 299) {
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+  } else {
+    // Handle errors
+    console.log(response.status, response.statusText);
+  }
+};
